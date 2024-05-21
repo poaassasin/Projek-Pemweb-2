22c8a925337f8a545a9fdb9da0abe637a0adead4
poaassasin <azkaujicoba@gmail.com>
2024-05-21 18:47:31 +0700

    Revert "add view Edutech"
    This reverts commit 7835a1e4fbce42bf0d03f708ec52f22bea784abc.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,7 +14,7 @@ class UserFactory extends Factory
     /**
      * The current password being used by the factory.
      */
-    protected static ?string $password ='12345';
+    protected static ?string $password;
 
     /**
      * Define the model's default state.
