a649a0ff5a734005330d3487a38594e63406728f
poaassasin <109660255+poaassasin@users.noreply.github.com>
2024-05-22 11:11:38 +0700

    Add files via upload

--- app/Http/Controllers/BelajarController.php
+++ app/Http/Controllers/BelajarController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\materi;
 
 class BelajarController extends Controller
 {
-    //
+    public function materi()
+    {
+        $materis = materi::all();
+        return view('edutech.detailMateri')
+        ->with('materis', $materis);
+    }
+    public function list()
+    {
+        $materis = materi::all();
+        return view('edutech.listMateri')
+        ->with('materis', $materis);
+    }
 }
--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\ulasan;
+use App\Models\course;
+
 
 class IndexController extends Controller
 {
-    //
+    public function dashboard()
+    {
+        $ulasans = ulasan::all();
+        $courses = course::all();
+        return view('edutech.dashboard')
+        ->with('ulasans', $ulasans)
+        ->with('courses', $courses);
+    }
 }
